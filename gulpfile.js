b51507fcff4337339a75e8793393e5417ba249bd
Nicu Stoian <andrei.nycu18@yahoo.com>
2020-09-12 17:44:31 +0300

    Adding function gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,29 @@
 const {src,dest} =  require('gulp');
 function css(){
-	return src("css/style.css").pipe(dest("img"));
+	return src("node_modules/bootstrap/dist/css/bootstrap.css").pipe(dest("css"));
 }
 exports.css = css;
+
+
+function allcss(){
+	return src("node_modules/@fortawesome/fontawesome-free/css/all.css").pipe(dest("css"));
+}
+exports.allcss = allcss;
+
+
+var source = ['fa-regular-400.svg','fa-regular-400.eot',  '.ttf woff woff2']
+
+function fonts(){
+	return src(["node_modules/@fortawesome/fontawesome-free/webfonts/fa-regular-400.eot"
+		,"node_modules/@fortawesome/fontawesome-free/webfonts/fa-regular-400.svg",
+		"node_modules/@fortawesome/fontawesome-free/webfonts/fa-regular-400.ttf",
+		"node_modules/@fortawesome/fontawesome-free/webfonts/fa-regular-400.woff",
+		"node_modules/@fortawesome/fontawesome-free/webfonts/fa-regular-400.woff2"])
+	.pipe(dest("css"));
+
+}
+exports.fonts = fonts;
+
+
+
+
