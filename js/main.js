acb6abe6a927ad33939b783cc758c16a2cc3adfb
Nicu Stoian <andrei.nycu18@yahoo.com>
2020-09-13 21:27:27 +0300

    Exercises json

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
 
+obiect = {"kind":"youtube#searchListResponse","etag":"\"m2yskBQFythfE4irbTIeOgYYfBU/PaiEDiVxOyCWelLPuuwa9LKz3Gk\"","nextPageToken":"CAUQAA","regionCode":"KE","pageInfo":{"totalResults":4249,"resultsPerPage":5},"items":[{"kind":"youtube#searchResult","etag":"\"m2yskBQFythfE4irbTIeOgYYfBU/QpOIr3QKlV5EUlzfFcVvDiJT0hw\"","id":{"kind":"youtube#channel","channelId":"UCJowOS1R0FnhipXVqEnYU1A"}},{"kind":"youtube#searchResult","etag":"\"m2yskBQFythfE4irbTIeOgYYfBU/AWutzVOt_5p1iLVifyBdfoSTf9E\"","id":{"kind":"youtube#video","videoId":"Eqa2nAAhHN0"}},{"kind":"youtube#searchResult","etag":"\"m2yskBQFythfE4irbTIeOgYYfBU/2dIR9BTfr7QphpBuY3hPU-h5u-4\"","id":{"kind":"youtube#video","videoId":"IirngItQuVs"}}]}
+
+console.log(obiect.regionCode);
+console.log(obiect.pageInfo.totalResults);
+console.log(obiect.items[0].id.channelId);
+console.log(obiect.items[0].etag);
+console.log(obiect.items[1].etag);
+
+
+
+
+
+
+
+
+
+
+/*
+
 function salut(){
 	console.log("Salut");
 }
@@ -30,3 +49,4 @@ ion = {varsta:20,
 
 
 console.log(ion.masini[1]);
+*/
