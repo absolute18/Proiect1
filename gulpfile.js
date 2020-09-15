817be2901090661e35e24603fa3eeed26d96482b
Nicu Stoian <andrei.nycu18@yahoo.com>
2020-09-15 19:49:10 +0300

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -2,4 +2,24 @@ const {src,dest} =  require('gulp');
 function css(){
 	return src("css/style.css").pipe(dest("img"));
 }
+
 exports.css = css;
+
+
+function allcss(){
+	return src("node_modules/@fortawesome/fontawesome-free/css/all.css").pipe(dest("css"));
+}
+exports.allcss = allcss;
+
+
+function fonts(){
+	return src(["node_modules/@fortawesome/fontawesome-free/webfonts/fa-regular-400.eot"
+		,"node_modules/@fortawesome/fontawesome-free/webfonts/fa-regular-400.svg",
+		"node_modules/@fortawesome/fontawesome-free/webfonts/fa-regular-400.ttf",
+		"node_modules/@fortawesome/fontawesome-free/webfonts/fa-regular-400.woff",
+		"node_modules/@fortawesome/fontawesome-free/webfonts/fa-regular-400.woff2"])
+	.pipe(dest("css"));
+
+}
+exports.fonts = fonts;
+
