b570794002895d131ec4d0aeca0bd9f16dcce028
Nicu Stoian <andrei.nycu18@yahoo.com>
2020-09-22 17:40:50 +0300

    Adding sass,jqurey and gulp function convert sass to css

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,21 @@
-const {src,dest} =  require('gulp');
+const {src,dest,watch} =  require('gulp');
+const sass = require('gulp-sass');
+
+function convertSass(){
+	return src("sass/style.scss").pipe(sass()).pipe(dest("css"));
+}
+
+function watchCode(){
+	 watch("sass/style.scss",convertSass);	
+}
+
 function css(){
 	return src("node_modules/bootstrap/dist/css/bootstrap.css").pipe(dest("css"));
 }
 
-exports.css = css;
-
-
 function allcss(){
 	return src("node_modules/@fortawesome/fontawesome-free/css/all.css").pipe(dest("css"));
 }
-exports.allcss = allcss;
-
 
 function fonts(){
 	return src(["node_modules/@fortawesome/fontawesome-free/webfonts/fa-regular-400.eot"
@@ -18,7 +23,17 @@ function fonts(){
 		"node_modules/@fortawesome/fontawesome-free/webfonts/fa-regular-400.ttf",
 		"node_modules/@fortawesome/fontawesome-free/webfonts/fa-regular-400.woff",
 		"node_modules/@fortawesome/fontawesome-free/webfonts/fa-regular-400.woff2"])
-	.pipe(dest("css"));
+	.pipe(dest("fonts"));
 
 }
+
+function js(){
+	return src("node_modules/jquery/dist/jquery.js").pipe(dest("js"));
+}
+
+exports.w = watchCode;
+exports.convertSass = convertSass;
+exports.js = js;
+exports.allcss = allcss;
+exports.css = css;
 exports.fonts = fonts;
---
+++ js/temajqurey.js
@@ -0,0 +1,16 @@
+/*$(document).ready(function(){
+ 	 $("button").click(function(){
+    $("div.text-about").hide();
+  });
+ });
+
+
+
+$(function(){
+	$("button").click(function(){
+		$('#box').fadeOut(1000);	
+	});
+
+//$(':contains("Johannes")').css('border','solid 1px red');
+$("div.text-about").css('border','solid 1px red');
+}); 	
